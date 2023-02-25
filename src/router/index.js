51de2e4008d35534a0d9bf6ed45a4360504f1339
Jdolan182 <38254684+Jdolan182@users.noreply.github.com>
2023-02-25 23:40:08 +0000

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/frontend/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +10,17 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/test',
+      name: 'for test api calls',
+      component: () => import('../views/admin/Test.vue')
+    },
+    {
+      path: '/peppermint',
+      name: 'peppermint',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/admin/Peppermint.vue')
     }
   ]
 })
---
+++ src/views/admin/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>Api data is: </h1>
+    <p id="api-data">   {{ apiData }}</p>
+  </div>
+</template>
+
+<script>
+    new Vue({
+    el: '#api-data',
+    data () {
+        return {
+        info: null
+        }
+    },
+    mounted () {
+        axios
+        .get('https://http://localhost/api/test')
+        .then(response => (this.info = response))
+    }
+    })
+</script>
