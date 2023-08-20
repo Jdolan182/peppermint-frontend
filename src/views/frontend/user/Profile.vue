62a78c45ccbaaa8e1f7fd964309e53c82d14a9e2
Jdolan182 <jordandolan182@gmail.com>
2023-08-20 22:39:25 +0100

    formatting

--- src/views/frontend/user/Profile.vue
+++ src/views/frontend/user/Profile.vue
@@ -1,144 +1,144 @@
 <template>
-          <!-- Settings forms -->
-          <div class="divide-y overflow-hidden bg-white shadow sm:rounded-lg">
-          <div class="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
-            <div>
-              <h2 class="text-base font-semibold leading-7">Personal Information</h2>
-              <p class="mt-1 text-sm leading-6 text-gray-400">Update your details</p>
+    <!-- Settings forms -->
+    <div class="divide-y overflow-hidden bg-white shadow sm:rounded-lg">
+    <div class="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
+      <div>
+        <h2 class="text-base font-semibold leading-7">Personal Information</h2>
+        <p class="mt-1 text-sm leading-6 text-gray-400">Update your details</p>
+      </div>
+
+      <Form 
+        @submit="updateDetails()"
+        class="md:col-span-2"
+      >
+        <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
+          <div class="text-center">
+            <ErrorLabel
+                :label="form.update.errorMessage"
+                :error="form.update.error"
+          />
+          </div>
+          <div class="sm:col-span-full">
+            <Label
+              for="name" 
+              label="Name"
+            />
+            <div class="mt-1">
+              <Input 
+                  name="name"
+                  placeholder="Name"
+                  :default-value="form.name.value"
+                  @input-value="(value) => (form.name.value = value)"
+                  :error="form.name.error"
+                  :error-message="form.name.errorMessage"
+              />
             </div>
-
-            <Form 
-              @submit="updateDetails()"
-              class="md:col-span-2"
-            >
-              <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
-                <div class="text-center">
-                  <ErrorLabel
-                      :label="form.update.errorMessage"
-                      :error="form.update.error"
-                />
-                </div>
-                <div class="sm:col-span-full">
-                  <Label
-                    for="name" 
-                    label="Name"
-                  />
-                  <div class="mt-1">
-                    <Input 
-                        name="name"
-                        placeholder="Name"
-                        :default-value="form.name.value"
-                        @input-value="(value) => (form.name.value = value)"
-                        :error="form.name.error"
-                        :error-message="form.name.errorMessage"
-                    />
-                  </div>
-                </div>
-
-                <div class="col-span-full">
-                  <Label
-                    for="email" 
-                    label="Email Address"
-                  />
-                  <div class="mt-1">
-                    <Input 
-                        name="email"
-                        placeholder="Email"
-                        :default-value="form.email.value"
-                        @input-value="(value) => (form.email.value = value)"
-                        :error="form.email.error"
-                        :error-message="form.email.errorMessage"
-                    />
-                  </div>
-                </div>
-
-              </div>
-
-              <div class="mt-8 flex w-14">
-                <Submit 
-                    label="Save"
-                  />
-              </div>
-            </Form>
           </div>
 
-          <div class="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
-            <div>
-              <h2 class="text-base font-semibold leading-7">Change password</h2>
-              <p class="mt-1 text-sm leading-6 text-gray-400">Update your password associated with your account.</p>
+          <div class="col-span-full">
+            <Label
+              for="email" 
+              label="Email Address"
+            />
+            <div class="mt-1">
+              <Input 
+                  name="email"
+                  placeholder="Email"
+                  :default-value="form.email.value"
+                  @input-value="(value) => (form.email.value = value)"
+                  :error="form.email.error"
+                  :error-message="form.email.errorMessage"
+              />
             </div>
+          </div>
 
-            <Form 
-              class="md:col-span-2"
-              @submit="updatePassword()"
-            >
-              <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
-                <div class="text-center">
-                  <ErrorLabel
-                      :label="passwordForm.update.errorMessage"
-                      :error="passwordForm.update.error"
-                />
-                </div>
-                <div class="col-span-full">
-                  <Label
-                    for="current_password" 
-                    label="Current Password"
-                  />
-                  <div class="mt-1">
-                    <Input 
-                        name="current_password"
-                        type="password"
-                        placeholder="Old Password"
-                        @input-value="(value) => (passwordForm.current_password.value = value)"
-                        :error="passwordForm.current_password.error"
-                        :error-message="passwordForm.current_password.errorMessage"
-                    />
-                  </div>
-                </div>
-
-                <div class="col-span-full">
-                  <Label
-                    for="password" 
-                    label="New Password"
-                  />
-                  <div class="mt-1">
-                    <Input 
-                        name="password"
-                        type="password"
-                        placeholder="New Password"
-                        @input-value="(value) => (passwordForm.password.value = value)"
-                        :error="passwordForm.password.error"
-                        :error-message="passwordForm.password.errorMessage"
-                    />
-                  </div>
-                </div>
-
-                <div class="col-span-full">
-                  <Label
-                    for="confirmPassword" 
-                    label="Confirm Password"
-                  />
-                  <div class="mt-1">
-                    <Input 
-                        name="confirmPassword"
-                        type="password"
-                        placeholder="Confirm Password"
-                        @input-value="(value) => (passwordForm.confirmPassword.value = value)"
-                        :error="passwordForm.confirmPassword.error"
-                        :error-message="passwordForm.confirmPassword.errorMessage"
-                    />
-                  </div>
-                </div>
-              </div>
-             
-              <div class="mt-8 flex w-14">
-                <Submit 
-                    label="Save"
-                  />
-              </div>
-            </Form>
-          </div>      
         </div>
+
+        <div class="mt-8 flex w-14">
+          <Submit 
+              label="Save"
+            />
+        </div>
+      </Form>
+    </div>
+
+    <div class="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
+      <div>
+        <h2 class="text-base font-semibold leading-7">Change password</h2>
+        <p class="mt-1 text-sm leading-6 text-gray-400">Update your password associated with your account.</p>
+      </div>
+
+      <Form 
+        class="md:col-span-2"
+        @submit="updatePassword()"
+      >
+        <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
+          <div class="text-center">
+            <ErrorLabel
+                :label="passwordForm.update.errorMessage"
+                :error="passwordForm.update.error"
+          />
+          </div>
+          <div class="col-span-full">
+            <Label
+              for="current_password" 
+              label="Current Password"
+            />
+            <div class="mt-1">
+              <Input 
+                  name="current_password"
+                  type="password"
+                  placeholder="Old Password"
+                  @input-value="(value) => (passwordForm.current_password.value = value)"
+                  :error="passwordForm.current_password.error"
+                  :error-message="passwordForm.current_password.errorMessage"
+              />
+            </div>
+          </div>
+
+          <div class="col-span-full">
+            <Label
+              for="password" 
+              label="New Password"
+            />
+            <div class="mt-1">
+              <Input 
+                  name="password"
+                  type="password"
+                  placeholder="New Password"
+                  @input-value="(value) => (passwordForm.password.value = value)"
+                  :error="passwordForm.password.error"
+                  :error-message="passwordForm.password.errorMessage"
+              />
+            </div>
+          </div>
+
+          <div class="col-span-full">
+            <Label
+              for="confirmPassword" 
+              label="Confirm Password"
+            />
+            <div class="mt-1">
+              <Input 
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="Confirm Password"
+                  @input-value="(value) => (passwordForm.confirmPassword.value = value)"
+                  :error="passwordForm.confirmPassword.error"
+                  :error-message="passwordForm.confirmPassword.errorMessage"
+              />
+            </div>
+          </div>
+        </div>
+        
+        <div class="mt-8 flex w-14">
+          <Submit 
+              label="Save"
+            />
+        </div>
+      </Form>
+    </div>      
+  </div>
 </template>
 
 <script setup>
