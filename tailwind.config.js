18f0bdf8b460183acb1595a878f3a5478efa159a
Jdolan182 <38254684+Jdolan182@users.noreply.github.com>
2025-02-18 21:21:37 +0000

    theme stuff
    - Need these in safelist to be able to load css - gonna need changing if I want to change theme at runtime with more themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,19 @@ module.exports = {
 
     },
   },
+  safelist: [
+    //TODO remove these
+   'focus:border-green-600',
+   'focus:ring-green-600',
+   'hover:bg-green-700',
+   'border-green-600',
+   'text-green-600',
+   'hover:bg-green-700',
+   'bg-green-700',
+   'bg-green-600',
+   'text-green-900',
+   'bg-green-900'
+  ],
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
